13556c9fd7ff71f32a39121415b844567f23a5d8
vidhya <mayasamudre@gmail.com>
2021-05-25 17:49:48 +0530

    to recheck conflicts i add line no5 as sign_in in mosels

--- mail/models.py
+++ mail/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Email(models.Model):
-
+    sign_in=models.CharField()
     username= models.CharField()
-    OTP=models.IntegerField
-    email=models.CharField()
-    img=models.IntegerField()
+    #OTP=models.IntegerField
+    #email=models.CharField()
+    #img=models.IntegerField()
 
 
